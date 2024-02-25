b23a1110b9021fe32bde6ee4fa9a8cc27a41070d
runethyne <runethyne@gmail.com>
2024-02-25 18:43:09 +0300

    coment

--- Assets/Scripts/EnterPoint.cs
+++ Assets/Scripts/EnterPoint.cs
@@ -11,7 +11,12 @@ public class EnterPoint : MonoBehaviour
     [SerializeField] private Settings _settings;
     [SerializeField] private Canvas _targetCanvas;
     private float _timeScale = 1;
-    
+    // Привет
+    /*/
+     * в
+     * в
+     * в
+     /*/
     void Start()
     {
         Time.timeScale = _timeScale;
