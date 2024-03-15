9b0d7cb661b4dab97f1f0dc83ce86ae3deb153bd
runethyne <runethyne@gmail.com>
2024-03-15 19:12:10 +0300

    ДЗ №3 / .raynorth

--- Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
+++ Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using Model.Runtime.Projectiles;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace UnitBrains.Player
@@ -12,18 +13,25 @@ namespace UnitBrains.Player
         private float _temperature = 0f;
         private float _cooldownTime = 0f;
         private bool _overheated;
-        
+
         protected override void GenerateProjectiles(Vector2Int forTarget, List<BaseProjectile> intoList)
         {
             float overheatTemperature = OverheatTemperature;
-            ///////////////////////////////////////
-            // Homework 1.3 (1st block, 3rd module)
-            ///////////////////////////////////////           
-            var projectile = CreateProjectile(forTarget);
-            AddProjectileToList(projectile, intoList);
-            ///////////////////////////////////////
-        }
+            float currentTemperature = GetTemperature();
+            float numProjectiles = currentTemperature;
 
+            if (currentTemperature >= overheatTemperature)
+            {
+                _overheated = true;
+                return;
+            }
+            IncreaseTemperature();
+            for (int i = 0; i < numProjectiles; i++)
+            {
+                var projectile = CreateProjectile(forTarget);
+                AddProjectileToList(projectile, intoList);
+            }
+        }
         public override Vector2Int GetNextStep()
         {
             return base.GetNextStep();
